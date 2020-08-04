f7e5608827a2a4de618aac8aa8a195758307ff5e
Kelin Field <kfield111@hotmail.com>
2020-08-04 22:03:04 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,15 @@
 def square_array(array)
+<<<<<<< HEAD
   new_array = []
   array.each do |number|
     new_array.push (number * number)
   end
   new_array
+=======
+  array.each do |value|
+    array.shift = original_number
+    squared_number = (original_number * original_number)
+    array.push squared_number
+  end
+>>>>>>> 2e6a3f73e421bbe9afa75f6b9edc6df762ae608c
 end
